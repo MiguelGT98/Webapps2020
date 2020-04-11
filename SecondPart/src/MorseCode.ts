1e47e0f55987735e89975f0e573ddc29829a8768
Miguel Gallardo <jgallardo9814@gmail.com>
2020-04-10 22:06:50 -0500

    2nd part of assignment complete.
    Page translates and shows morse code using a lightbulb.
    Also show morse code text below lightbulb.

---
+++ SecondPart/src/MorseCode.ts
@@ -0,0 +1,105 @@
+window.onload = () => {
+  const searchForm = document.getElementById("search-form");
+  const text = document.getElementById("text");
+  const timeBetweenDigit = document.getElementById("timeBetweenDigit");
+  const timeBetweenLetters = document.getElementById("timeBetweenLetters");
+  const timeBetweenWords = document.getElementById("timeBetweenWords");
+
+  searchForm.onsubmit = (e) => {
+    e.preventDefault();
+
+    morseCodeProgression(
+      text["value"],
+      timeBetweenDigit["value"],
+      timeBetweenLetters["value"],
+      timeBetweenWords["value"]
+    );
+  };
+};
+
+async function morseCodeProgression(
+  text: string,
+  timeBetweenDigit: number,
+  timeBetweenLetters: number,
+  timeBetweenWords: number
+) {
+  const morseCode: any = {
+    a: "·-",
+    b: "-···",
+    c: "-·-·",
+    d: "-··",
+    e: "·",
+    f: "··-·",
+    g: "--·",
+    h: "····",
+    i: "··",
+    j: "·---",
+    k: "-·-",
+    l: "·-··",
+    m: "--",
+    n: "-·",
+    o: "---",
+    p: "·--·",
+    q: "--·-",
+    r: "·-·",
+    s: "···",
+    t: "-",
+    u: "··-",
+    v: "···-",
+    w: "·--",
+    x: "-··-",
+    y: "-·--",
+    z: "--··",
+    1: "·----",
+    2: "··---",
+    3: "···--",
+    4: "····-",
+    5: "·····",
+    6: "-····",
+    7: "--···",
+    8: "---··",
+    9: "----·",
+    0: "-----",
+  };
+
+  let index: number = 0;
+
+  const morseWrapper = document.getElementById("morse-wrapper");
+  morseWrapper.innerHTML = "";
+  const lightbulb = document.getElementById("lightbulb");
+
+  for (let i = 0; i < text.length; i++) {
+    const outerSpan = document.createElement("span");
+    outerSpan.classList.add("outer-span");
+    morseWrapper.appendChild(outerSpan);
+
+    if (text[i].toLowerCase() === " ") {
+      await sleep(timeBetweenWords);
+      continue;
+    }
+
+    const code = morseCode[text[i].toLowerCase()];
+    for (let j = 0; j < code.length; j++) {
+      const span = document.createElement("span");
+      span.classList.add("inner-span");
+      span.dataset["index"] = `${index}`;
+      span.innerHTML = code[j];
+      outerSpan.appendChild(span);
+      index++;
+
+      if (code[j] === "·") lightbulb.classList.add("dot");
+      if (code[j] === "-") lightbulb.classList.add("line");
+
+      await sleep(timeBetweenDigit);
+
+      lightbulb.classList.remove("dot");
+      lightbulb.classList.remove("line");
+    }
+
+    await sleep(timeBetweenLetters);
+  }
+}
+
+const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
